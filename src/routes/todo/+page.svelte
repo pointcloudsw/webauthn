d94c9f0b16aa89366e5a5bd92351c7910071a084
jrollio <jrollio@outlook.com>
2025-11-18 20:22:31 +0000

    feat: list items crud modal

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
 import { createList, getLists, deleteList, listUpdate, updateList } from './data.remote';
-import EditListModal from './EditListModalOther.svelte';
-import { boolState } from '$lib/state.svelte';
 
+
+// import { DOMAttributes } from 'svelte/elements';
+
+// let dialogCmd = new EventTarget();
+// dialogCmd.addEventListener('oncommand', ()=>)
 let { data } = $props();
 let { userId, username } = data;
-let bigEvent = $state('');
+// let btnEvtData = $state('');
+const btnEvtData = $state({
+	dataset: {},
+	owner: '',
+	id: '',
+	editable: false,
+	created: '',
+	items: []
+});
+
 createList.fields.created.set((new Date()).toString());
 createList.fields.editable.set(true);
 createList.fields.owner.set(userId);
@@ -17,14 +29,17 @@ let createListDialog: HTMLDialogElement = $state() as HTMLDialogElement; // HTML
 $effect(() => {
 	if (createListModal?.value) createListDialog.showModal();
 });
-
-const updModal = boolState();
-
-let updateListModal = $state({value: false});
+const updateListModalState = $state({value: false});
 let updateListDialog: HTMLDialogElement = $state() as HTMLDialogElement; // HTMLDialogElement
 $effect(() => {
-	if (updModal.get()) updateListDialog.showModal();
-});
+	if ( updateListModalState?.value === true )
+		updateListDialog?.showModal();
+	else
+		updateListDialog?.close();
+	}
+);
+
+
 
 /*
 const editList = (e:any) => {
@@ -60,51 +75,72 @@ const editList = (e:any) => {
 <main>
 	{#if userId}
 
-		<section class="lists">
-			<h1>Todo Lists</h1>
 
-				<div class="todo-list">
-					<dialog id="updateListDialog" oncommand={(e:any)=>bigEvent=e.source.dataset}>
-						<!-- <EditListModal {bigEvent} /> -->
-						<form {...listUpdate} id="updateListForm" name="updateListForm" >
-							<label for="owner">Owner: {bigEvent.owner}</label>
-							<label for="id">ID: {bigEvent.list_id}</label>
-							<!-- <input name="owner" type="readonly" value={bigEvent.owner} /> -->
-							<!-- </label> -->
-							<label for="created">Created: {bigEvent.created}</label>
-							<label for="modified">Modified: {bigEvent.created}</label>
+					<!-- <dialog id="updateListDialog" onclick={(e:any)=>{ alert('clicked'); btnEvtData=e.source.dataset}} onclose={()=>updateListModal.value=false}> -->
+					<!-- <dialog id="updateListDialog" onsubmit={(e:any)=>{ btnEvtData=e.source.dataset;console.log(e); }}> -->
+					<!-- <dialog id="updateListDialog" bind:this={updateListDialog} onsubmit={(e:any)=>{ btnEvtData = e.submitter.dataset; updateListModal.value = true;}}> -->
+					<!-- <dialog id="updateListDialog" bind:this={updateListDialog} onclose={()=> (updateListModal.value=false)} onsubmit={(e:any)=>{ console.log(e); btnEvtData = e.submitter.dataset.owner;updateListModal.value = true;  console.log(e.submitter.dataset); console.log(btnEvtData); console.log(updateListModal.value); console.log(updateListDialog)}}> -->
+					<!-- <dialog id="updateListDialog" onclose={()=> (updateListModalState.value=false)} onsubmit={(e:any)=>{ console.log(e); btnEvtData = e.submitter.dataset.owner;updateListModalState.value = true;  console.log(e.submitter.dataset); console.log(btnEvtData);}}> -->
+					<dialog id="updateListDialog" bind:this={updateListDialog} onsubmit={(e:any)=>{updateListModalState.value=false}}>
+
+						<form {...listUpdate} id="updateListForm" name="updateListForm">
+							<label for="owner">Owner: {btnEvtData.owner || ''}</label>
+							<label for="id">ID: {btnEvtData.id || ''}</label>
+							<label for="created">Created: {btnEvtData?.created || ''}</label>
+							<label for="modified">Modified: {btnEvtData?.created || ''}</label>
 							<label for="editable">Editable:
-							<input name="editable" type="checkbox" value={bigEvent.editable} />
+							<input name="editable" type="checkbox" value={btnEvtData?.editable || false } />
 							</label>
-							<label for="items">Items:
-							<input name="items" value={bigEvent.items} />
-							</label>
-							
+							<section>
+							<!-- <label for="items">Items:</label> -->
+							<h3>Items:</h3>
+							<input name="items" type="readonly" value={btnEvtData?.items || ''} />
+
+							<!-- {#each btnEvtData.items as i}
+							<label for="item_id">Item ID:</label>
+							<input name="item_id" type="readonly" value={i.id || ''} />
+							<label for="item_txt">Item:</label>
+							<input name="item_txt" type="textbox" value={i.text || ''} />
+							<label for="item_editable">Editable:</label>
+							<input name="item_editable" type="checkbox" value={i.editable || ''} />
+							{/each} -->
+							</section>
+							<!-- <button onclick={()=>updateListModalState.value = false && this.close()}>OK</button> -->
 							<button>OK</button>
 						</form>
 					</dialog>
 					<form {...deleteList} name="deleteListForm" id="deleteListForm"></form>
 
+		<section class="lists">
+			<h1>Todo Lists</h1>
+
+				<div class="todo-list">
+
 					{#each await getLists(userId) as list (list)}
 					<div data-list_id={list.id}>
 						<p data-list_id={list.id}>{list.id}</p>
 						<p data-list_title={list.title}>{list.title}</p>
-						<p data-list_owner={list.owner}>{list.owner}</p>
+						<p data-list_created={list.created}>{list.created}</p>
 						<p data-list_owner={list.owner}>{list.owner}</p>
 
 						
 						<button name="deleteListFormButton" form="deleteListForm" type="submit" value="{userId},{list.id}">DELETE</button>
 						
 
-						<button type="submit" name="updateListFormButton" command="show-modal" commandfor="updateListDialog" data-list_id={list.id}  data-owner={userId} data-items={list.items}  data-editable={list.editable} data-created={list.created}>Edit List</button>
+						<!-- <button type="button" name="updateListFormButton" command="show-modal" commandfor="updateListDialog" data-list_id={list.id}  data-owner={userId} data-items={list.items}  data-editable={list.editable} data-created={list.created}>Edit List</button> -->
+						<!-- <button name="updateListFormButton" form="updateListForm" data-list_id={list.id}  data-owner={userId} data-items={list.items}  data-editable={list.editable} data-created={list.created}>Edit List</button> -->
+						<button name="updateListFormButton" onclick={(e:any)=>{updateListModalState.value = true; btnEvtData.dataset = e.target.dataset; btnEvtData.id = e.target.dataset['list_id']; btnEvtData.owner = e.target.dataset['owner']; btnEvtData.created = e.target.dataset['created']; btnEvtData.editable = e.target.dataset['editable']; btnEvtData.items = e.target.dataset['items'];}} data-list_id={list.id}  data-owner={userId} data-items={JSON.stringify(list?.items)}  data-editable={list.editable} data-created={list.created}>Edit List</button>
 
 
 						<div data-list_items={`list-${list.id}-items`} class="todo-items">
+							<h3>Array?:{Array.isArray(list?.items)}</h3>
+							<h5>Type?:{typeof list?.items}</h5>
+							<h5>Values?:{JSON.stringify(list?.items)}</h5>
 						{#each list?.items as i}
-							<p data-list_item_id={i.id}>{i.id}</p>
-							<p data-list_item_text={i.text}>{i.text}</p>
-							<p data-list_item_created={i.created}>{i.created}</p>
-							<p data-list_item_editable={i.editable}>{i.editable}</p>
+							<p data-list_item_id={i.id}>Item ID: {i.id}</p>
+							<p data-list_item_text={i.text}>Item Text: {i.text}</p>
+							<p data-list_item_created={i.created}>Item Created: {i.created}</p>
+							<p data-list_item_editable={i.editable}>Item is Editable: {i.editable}</p>
 						{/each}
 						</div>
 					</div>
